a83896ff87d1b85fb39b67d9c19ff45a69637cbc
Alex <sasha.k150189@mail.ru>
2021-08-05 23:07:15 +0300

    done 1 and 2 task

--- Homework 2/Program.cs
+++ Homework 2/Program.cs
@@ -6,7 +6,50 @@ namespace Homework_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            /*
+            int x;
+            Console.WriteLine("Введите целое число.");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x + " - " + ((x % 2 == 0) ? "четное число" : "нечетное число"));
+            Console.WriteLine("Для выхода из приложение нажмите Enter");
+            Console.ReadLine();*/
+
+            /*Task 1 - Написать метод, возвращающий минимальное из трёх чисел.
+            int x;
+            Console.WriteLine("Введите первое число");
+            x = Convert.ToInt32(Console.ReadLine());
+            int b;
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToInt32(Console.ReadLine());
+            int c;
+            Console.WriteLine("Введите третье число");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (x < b && x < c)
+            {
+                Console.WriteLine("Минимальное число " + x);
+            }
+            else if (b < c)
+            {
+                Console.WriteLine("Минимальное число " + b);
+            }
+            else
+            {
+                Console.WriteLine("Минимальное число " + c);
+            }
+
+            //Task 2 - Написать метод подсчета количества цифр числа.
+            int a;
+            int counter = 0;
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+            while(a > 0)
+            {
+                a = a / 10;
+                Console.WriteLine(a);
+                counter++;
+            }
+            Console.WriteLine("Число содержит " + counter + " цифр");
         }
     }
 }
