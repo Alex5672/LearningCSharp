972b68cbc6b3e7ccb07e135987202c8e2036a76e
Alex <sasha.k150189@mail.ru>
2021-08-10 15:56:41 +0300

    add task 2

--- Homework 3 _Kondyukov/Program.cs
+++ Homework 3 _Kondyukov/Program.cs
@@ -10,11 +10,11 @@ namespace Homework_3__Kondyukov
             a = re;
             b = im;
         }
-        public compleks Sum(compleks x, compleks y)
+        public static compleks Sum(compleks x, compleks y)
         {
             return new compleks(re: x.a + y.a, im: x.b + y.b);
         }
-        public compleks Min(compleks x, compleks y)
+        public static compleks Min(compleks x, compleks y)
         {
             return new compleks(re: x.a - y.a, im: x.b - y.b);
         }
@@ -32,11 +32,28 @@ namespace Homework_3__Kondyukov
             Console.WriteLine(z1);
             compleks z2 = new compleks(re: 20, im: 15);
             Console.WriteLine(z2);
-            compleks z3 = z1.Sum(z1, z2);//сложение
+            compleks z3 = compleks.Sum(z1, z2);//сложение
             Console.WriteLine(z3);
-            compleks z4 = z1.Min(z1, z2);//вычитание
+            compleks z4 = compleks.Min(z1, z2);//вычитание
             Console.WriteLine(z4);
 
+            //Task 2 - С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).
+            //Требуется подсчитать сумму всех нечётных положительных чисел.
+            //Сами числа и сумму вывести на экран, используя tryParse
+
+            int a;
+            int k = 0;
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+            while (a != 0)
+            {
+                if (a > 0 && a % 2 != 0)
+                {
+                    k += a;
+                }
+                a = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(k);
         }
     }
 }
