b8d7b9f85886d1d151cdbfd187cf75ec4f322b38
Alex <sasha.k150189@mail.ru>
2021-08-12 18:35:35 +0300

    add new project

---
+++ Homework 4 _Kondyukov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_4__Kondyukov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
