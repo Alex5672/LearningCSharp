4d10c9a7ff303e70c5b8b235855b7dc3ecf053b5
Alex <sasha.k150189@mail.ru>
2021-08-14 11:12:19 +0300

    add new project HW 5

--- Homework 4 _Kondyukov/Program.cs
+++ Homework 4 _Kondyukov/Program.cs
@@ -4,6 +4,7 @@ namespace Homework_4__Kondyukov
 {
     class Program
     {
+        #region Draft for Homework
         /*int[][] rows;
         int rowCount;
         public Massive (int rowCount) {this.rowCount = rowCount;}
@@ -36,106 +37,155 @@ namespace Homework_4__Kondyukov
             }
         }
     }*/
-        public static void Main(string[] args)
+        #endregion
+        /* Task 2 - 2. Реализуйте задачу 1 в виде статического класса StaticClass;
+        а) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1 
+        class Task2
+           {
+               int[] mass;
+               int lo;
+               int pair;
+               Random rnd = new Random();
+               public Task2(int lo) { this.lo = lo; }
+               public void Print()
+               {
+                   for (int i = 0; i < mass.Length; i++)
+                   {
+                       mass[i] = rnd.Next(100,200);
+                       Console.Write($"{mass[i],5} ");
+                   }
+               }
+               public void Sort()
+               {
+                   Array.Sort(mass);
+                   Console.WriteLine();
+                   for (int i = 0; i < mass.Length; i++)
+                   {
+                       Console.Write($"{mass[i],5} ");
+                   }
+                   Console.WriteLine();
+               }
+               public void Init()
+               {
+                   mass = new int[lo];
+                   for (int i = 0; i < mass.Length - 1; i++)
+                   {
+                       if (mass[i] % 3 == 0 && mass[i + 1] % 3 != 0 || mass[i] % 3 != 0 && mass[i + 1] % 3 == 0)
+                       {
+                           pair++;
+                       }
+                       else { }
+                   }
+                   Console.WriteLine("Amount of searching objects is " + pair);
+               }
+           }*/
+           public static void Main(string[] args)
+           {
+               /*
+               Task2 pt = new Task2(10);
+               pt.Init();
+               pt.Print();
+               pt.Sort();*/
+
+
+               //Task 1 - Дан целочисленный массив из 20 элементов. Элементы массива могут принимать целые значения
+               //от –10 000 до 10 000 включительно. Заполнить случайными числами.
+               //Написать программу, позволяющую найти и вывести количество пар элементов массива,
+               //в которых только одно число делится на 3. В данной задаче под парой подразумевается два подряд идущих элемента массива.
+               //Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2.
+
+               int[] Massive; //создал переменную типа массив
+               Massive = new int[20]; //определили 20 элементов для массива
+               Random rnd = new Random(); //создание объекта для генерации чисел
+               for (int i = 0; i < Massive.Length; i++)
+               {
+                   Massive[i] = rnd.Next(-10000, 10000); //ограничил генерацию рандомных чисел
+                   Console.Write($"{Massive[i],5} ");
+               }
+               /*Array.Sort(Massive); //вроде как все методы работают с отсортированными массивами, значит сортируем
+               Console.WriteLine();
+               for (int i = 0; i < Massive.Length; i++)
+               {
+                   Console.Write($"{Massive[i],5} ");
+               }*/
+               Console.WriteLine();
+               int pair = 0;
+               for (int i = 0; i < Massive.Length-1; i++)
+               {
+                   if (Massive[i] % 3 == 0 && Massive[i + 1] % 3 != 0 || Massive[i] % 3 != 0 && Massive[i + 1] % 3 == 0)
+                   {
+                       pair++;
+                   }
+                   else {}
+               }
+               Console.WriteLine("Amount of searching objects is " + pair);
+
+        #region Helping information from lesson
+        //Massive pt = new Massive(11);
+        //pt.Init();
+        //pt.Print();
+
+
+
+        /*//Ступенчатый массив, массив массивов
+        int[][] triangle = new int[5][];
+        //triangle[0] = new int [1] - определили на ппервой строчке 1 элемент массива
+        for (int i = 0; i < triangle.Length; i++) //опредилили элементы массива автоматически
         {
-            //Task 1 - Дан целочисленный массив из 20 элементов. Элементы массива могут принимать целые значения
-            //от –10 000 до 10 000 включительно. Заполнить случайными числами.
-            //Написать программу, позволяющую найти и вывести количество пар элементов массива,
-            //в которых только одно число делится на 3. В данной задаче под парой подразумевается два подряд идущих элемента массива.
-            //Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2.
-
-            int[] Massive; //создал переменную типа массив
-            Massive = new int[20]; //определили 20 элементов для массива
-            Random rnd = new Random(); //создание объекта для генерации чисел
-            for (int i = 0; i < Massive.Length; i++)
-            {
-                Massive[i] = rnd.Next(-10000, 10000); //ограничил генерацию рандомных чисел
-                Console.Write($"{Massive[i],5} ");
-            }
-            /*Array.Sort(Massive); //вроде как все методы работают с отсортированными массивами, значит сортируем
-            Console.WriteLine();
-            for (int i = 0; i < Massive.Length; i++)
-            {
-                Console.Write($"{Massive[i],5} ");
-            }*/
-            Console.WriteLine();
-            int pair = 0;
-            for (int i = 0; i < Massive.Length-1; i++)
-            {
-                if (Massive[i] % 3 == 0 && Massive[i + 1] % 3 != 0 || Massive[i] % 3 != 0 && Massive[i + 1] % 3 == 0)
-                {
-                    pair++;
-                }
-                else {}
-            }
-            Console.WriteLine("Amount of searching objects is " + pair);
-
-
-            //Massive pt = new Massive(11);
-            //pt.Init();
-            //pt.Print();
-
-
-
-            /*//Ступенчатый массив, массив массивов
-            int[][] triangle = new int[5][];
-            //triangle[0] = new int [1] - определили на ппервой строчке 1 элемент массива
-            for (int i = 0; i < triangle.Length; i++) //опредилили элементы массива автоматически
-            {
-                triangle[i] = new int [i + 1];
-            }
-            for (int i = 0; i < triangle.Length; i++)
-            {
-                for(int u = 0; u < triangle[i].Length; u++)
-                {
-                    Console.Write($"{triangle[i][u]} ");
-                }
-                Console.WriteLine();
-            }*/
-
-            //Многомерные массивы
-            /*
-            int[,] matrix;
-            matrix = new int[10, 20];
-            Console.WriteLine($"Rank = {matrix.Rank}");
-            Console.WriteLine($"Getlength(0) = {matrix.GetLength(0)}");
-            Console.WriteLine($"Getlength(1) = {matrix.GetLength(1)}");
-            matrix[3, 4] = 99;
-            matrix[2, 3] = 99;
-            matrix[1, 2] = 99;
-            matrix[4, 5] = 99;
-            matrix[5, 6] = 99;
-            matrix[0, 1] = 99;
-            matrix[6, 7] = 99;
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int u = 0; u < matrix.GetLength(1); u++)
-                {
-                    Console.Write($"{matrix[i, u],5}");
-                }
-                Console.WriteLine();
-            }*/
-
-            //Одномерные массивы
-            /*int[] Massive;
-            Massive = new int[10];
-            Massive[1] = 10;
-            Random r = new Random();
-            for (int i = 0; i < 10; i++)
-            {
-                Massive[i] = r.Next(10);
-                Console.Write($"{Massive[i]} ");
-            }
-            Array.Sort(Massive);
-            Console.WriteLine();
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write($"{Massive[i]} ");
-            }*/
-
-
+            triangle[i] = new int [i + 1];
         }
+        for (int i = 0; i < triangle.Length; i++)
+        {
+            for(int u = 0; u < triangle[i].Length; u++)
+            {
+                Console.Write($"{triangle[i][u]} ");
+            }
+            Console.WriteLine();
+        }*/
+
+        //Многомерные массивы
+        /*
+        int[,] matrix;
+        matrix = new int[10, 20];
+        Console.WriteLine($"Rank = {matrix.Rank}");
+        Console.WriteLine($"Getlength(0) = {matrix.GetLength(0)}");
+        Console.WriteLine($"Getlength(1) = {matrix.GetLength(1)}");
+        matrix[3, 4] = 99;
+        matrix[2, 3] = 99;
+        matrix[1, 2] = 99;
+        matrix[4, 5] = 99;
+        matrix[5, 6] = 99;
+        matrix[0, 1] = 99;
+        matrix[6, 7] = 99;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int u = 0; u < matrix.GetLength(1); u++)
+            {
+                Console.Write($"{matrix[i, u],5}");
+            }
+            Console.WriteLine();
+        }*/
+
+        //Одномерные массивы
+        /*int[] Massive;
+        Massive = new int[10];
+        Massive[1] = 10;
+        Random r = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            Massive[i] = r.Next(10);
+            Console.Write($"{Massive[i]} ");
+        }
+        Array.Sort(Massive);
+        Console.WriteLine();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"{Massive[i]} ");
+        }*/
+        #endregion
+
     }
+}
     }
 
 
---
+++ Homework 5 _Kondyukov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_5__Kondyukov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
